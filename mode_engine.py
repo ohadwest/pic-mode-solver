e3af5000e44c27a610c11cd7a53b35b29c01b246
ohadwest <ohadwest@gmail.com>
2026-08-04 01:25:22 +0300

    Update run_single_point to handle polarization
    Refactor run_single_point to include polarization choice and clean up commented code.

--- mode_engine.py
+++ mode_engine.py
@@ -50,11 +50,11 @@ def get_core_index(lam_um, material_name):
     else:
         return n_sin_stoch(lam_um)
 
-# --- MESH GENERATION & INTEGRATION MASKS ---
+# --- MESH GENERATION ---
 
 def build_advanced_mesh(w_core, h_core, bottom_ox, top_ox, side_margin, dx, dy, n_core, n_clad):
     total_width = w_core + 2 * side_margin
-    total_height = bottom_ox + h_core + top_ox + 1.5  # Extra space for air cladding
+    total_height = bottom_ox + h_core + top_ox + 1.5
     
     nx = int(np.round(total_width / dx)) + 1
     ny = int(np.round(total_height / dy)) + 1
@@ -67,21 +67,21 @@ def build_advanced_mesh(w_core, h_core, bottom_ox, top_ox, side_margin, dx, dy, 
     
     eps = np.full((len(xc), len(yc)), n_clad**2)
     
-    # Core Region
     x_min, x_max = -w_core / 2.0, w_core / 2.0
     y_min, y_max = bottom_ox, bottom_ox + h_core
     core_mask = (xc[:, None] >= x_min) & (xc[:, None] <= x_max) & \
                 (yc[None, :] >= y_min) & (yc[None, :] <= y_max)
     eps[core_mask] = n_core**2
     
-    # Air Cladding Boundary (Above Oxide Top)
     interface_y = bottom_ox + h_core + top_ox
-    air_mask = yc[None, :] > interface_y
-    eps[np.repeat(air_mask, len(xc), axis=0)] = 1.0**2  # Air n=1
+    air_mask_1d = yc > interface_y
+    eps[:, air_mask_1d] = 1.0**2  # Air
+    
+    air_mask = np.repeat(air_mask_1d[None, :], len(xc), axis=0)
     
     return xc, yc, eps, core_mask, air_mask, interface_y, x_min, x_max, y_min, y_max
 
-# --- RIGOROUS 2D SVFD EIGENMODE SOLVER (TE & TM) ---
+# --- 2D SVFD SOLVER ---
 
 def svmodes_2d(lam_um, guess, nmodes, dx, dy, eps_mesh, polarization='ex'):
     nx, ny = eps_mesh.shape
@@ -102,7 +102,6 @@ def svmodes_2d(lam_um, guess, nmodes, dx, dy, eps_mesh, polarization='ex'):
     q_mat = np.full((nx, ny), dy)
     
     if polarization.lower() == 'ex':
-        # Quasi-TE Mode Solver (Ex main component)
         an = 2.0 / (n_mat * (n_mat + s_mat))
         as_ = 2.0 / (s_mat * (n_mat + s_mat))
         num_e = 8.0 * (p_mat * (ep - ew) + 2.0 * w_mat * ew) * ee
@@ -113,7 +112,6 @@ def svmodes_2d(lam_um, guess, nmodes, dx, dy, eps_mesh, polarization='ex'):
         aw = num_w / den_e
         ap = ep * (k0**2) - an - as_ - ae * (ep / ee) - aw * (ep / ew)
     else:
-        # Quasi-TM Mode Solver (Ey main component)
         num_n = 8.0 * (q_mat * (ep - es) + 2.0 * s_mat * es) * en
         den_n = (q_mat * (ep - en) + 2.0 * n_mat * en) * (q_mat**2 * (ep - es) + 4.0 * s_mat**2 * es) + \
                 (q_mat * (ep - es) + 2.0 * s_mat * es) * (q_mat**2 * (ep - en) + 4.0 * n_mat**2 * en)
@@ -154,122 +152,30 @@ def svmodes_2d(lam_um, guess, nmodes, dx, dy, eps_mesh, polarization='ex'):
 
 # --- SIMULATION ENGINES ---
 
-def run_single_point(w_core, h_core, bottom_ox, top_ox, lam_um, res_mode, core_material):
+def run_single_point(w_core, h_core, bottom_ox, top_ox, lam_um, res_mode, core_material, pol_choice="Both"):
     dx = dy = 0.005 if "hr" in res_mode else (0.01 if "mr" in res_mode else 0.02)
     n_core = get_core_index(lam_um, core_material)
     n_clad = sellmeier_sio2(lam_um)
     
-    xc, yc, eps_mesh, core_mask, air_mask_1d, interface_y, x_min, x_max, y_min, y_max = build_advanced_mesh(
+    xc, yc, eps_mesh, core_mask, air_mask, interface_y, x_min, x_max, y_min, y_max = build_advanced_mesh(
         w_core, h_core, bottom_ox, top_ox, 2.0, dx, dy, n_core, n_clad
     )
     
-    air_mask = np.repeat(air_mask_1d, len(xc), axis=0)
-    
-    # TE Mode Calculation
-    guess_te = (n_core + n_clad) / 2.0
-    phi_te, neff_te = svmodes_2d(lam_um, guess_te, 1, dx, dy, eps_mesh, 'ex')
-    p_te = phi_te[:, :, 0]
-    total_p_te = np.sum(p_te**2)
-    gamma_core_te = (np.sum(p_te[core_mask]**2) / total_p_te) * 100.0 if total_p_te > 0 else 0.0
-    gamma_air_te = (np.sum(p_te[air_mask]**2) / total_p_te) * 100.0 if total_p_te > 0 else 0.0
-    
-    # TM Mode Calculation (Independent Ey Solver)
-    guess_tm = (n_core + 2*n_clad) / 3.0  # Slightly lower guess for TM fundamental
-    phi_tm, neff_tm = svmodes_2d(lam_um, guess_tm, 1, dx, dy, eps_mesh, 'ey')
-    p_tm = phi_tm[:, :, 0]
-    total_p_tm = np.sum(p_tm**2)
-    gamma_core_tm = (np.sum(p_tm[core_mask]**2) / total_p_tm) * 100.0 if total_p_tm > 0 else 0.0
-    gamma_air_tm = (np.sum(p_tm[air_mask]**2) / total_p_tm) * 100.0 if total_p_tm > 0 else 0.0
-    
-    return {
+    res = {
         'xc': xc, 'yc': yc, 'eps_mesh': eps_mesh,
-        'phi_te': p_te, 'phi_tm': p_tm,
-        'neff_te': neff_te[0], 'neff_tm': neff_tm[0],
-        'gamma_core_te': gamma_core_te, 'gamma_core_tm': gamma_core_tm,
-        'gamma_air_te': gamma_air_te, 'gamma_air_tm': gamma_air_tm,
         'x_min': x_min, 'x_max': x_max, 'y_min': y_min, 'y_max': y_max,
-        'interface_y': interface_y, 'lam_um': lam_um
+        'interface_y': interface_y, 'lam_um': lam_um, 'pol_choice': pol_choice
     }
-
-def run_1d_sweep(param_name, param_vec, fixed_params, res_mode, core_material, progress_callback=None):
-    n_pts = len(param_vec)
-    neff_te_vec = np.zeros(n_pts)
-    neff_tm_vec = np.zeros(n_pts)
-    gamma_core_te_vec = np.zeros(n_pts)
-    gamma_core_tm_vec = np.zeros(n_pts)
-    gamma_air_te_vec = np.zeros(n_pts)
-    gamma_air_tm_vec = np.zeros(n_pts)
     
-    for i, val in enumerate(param_vec):
-        if progress_callback:
-            progress_callback(i + 1, n_pts)
-            
-        p_dict = fixed_params.copy()
-        p_dict[param_name] = val
+    if pol_choice in ["TE", "Both"]:
+        guess_te = (n_core + n_clad) / 2.0
+        phi_te, neff_te = svmodes_2d(lam_um, guess_te, 1, dx, dy, eps_mesh, 'ex')
+        p_te = phi_te[:, :, 0]
+        tot_te = np.sum(p_te**2)
+        res['phi_te'] = p_te
+        res['neff_te'] = neff_te[0]
+        res['gamma_core_te'] = (np.sum(p_te[core_mask]**2) / tot_te) * 100.0 if tot_te > 0 else 0.0
+        res['gamma_air_te'] = (np.sum(p_te[air_mask]**2) / tot_te) * 100.0 if tot_te > 0 else 0.0
+        res['a_eff_te'] = ((np.sum(p_te**2) * dx * dy)**2) / (np.sum(p_te**4) * dx * dy)
         
-        w_c = p_dict['Waveguide Width']
-        h_c = p_dict['Waveguide Height']
-        lam = p_dict['Wavelength']
-        t_ox = p_dict['Oxide Top Thickness']
-        b_ox = p_dict['Oxide Bottom Thickness']
-        
-        res = run_single_point(w_c, h_c, b_ox, t_ox, lam, res_mode, core_material)
-        
-        neff_te_vec[i] = res['neff_te']
-        neff_tm_vec[i] = res['neff_tm']
-        gamma_core_te_vec[i] = res['gamma_core_te']
-        gamma_core_tm_vec[i] = res['gamma_core_tm']
-        gamma_air_te_vec[i] = res['gamma_air_te']
-        gamma_air_tm_vec[i] = res['gamma_air_tm']
-        
-    return {
-        'param_name': param_name, 'param_vec': param_vec,
-        'neff_te_vec': neff_te_vec, 'neff_tm_vec': neff_tm_vec,
-        'gamma_core_te_vec': gamma_core_te_vec, 'gamma_core_tm_vec': gamma_core_tm_vec,
-        'gamma_air_te_vec': gamma_air_te_vec, 'gamma_air_tm_vec': gamma_air_tm_vec
-    }
-
-def run_2d_universal_sweep(param1_name, vec1, param2_name, vec2, fixed_params, res_mode, core_material, progress_callback=None):
-    n1, n2 = len(vec1), len(vec2)
-    total_sims = n1 * n2
-    
-    neff_te_mat = np.zeros((n2, n1))
-    neff_tm_mat = np.zeros((n2, n1))
-    gamma_core_te_mat = np.zeros((n2, n1))
-    gamma_core_tm_mat = np.zeros((n2, n1))
-    gamma_air_te_mat = np.zeros((n2, n1))
-    gamma_air_tm_mat = np.zeros((n2, n1))
-    
-    count = 0
-    for j, val2 in enumerate(vec2):
-        for i, val1 in enumerate(vec1):
-            count += 1
-            if progress_callback:
-                progress_callback(count, total_sims)
-                
-            p_dict = fixed_params.copy()
-            p_dict[param1_name] = val1
-            p_dict[param2_name] = val2
-            
-            w_c = p_dict['Waveguide Width']
-            h_c = p_dict['Waveguide Height']
-            lam = p_dict['Wavelength']
-            t_ox = p_dict['Oxide Top Thickness']
-            b_ox = p_dict['Oxide Bottom Thickness']
-            
-            res = run_single_point(w_c, h_c, b_ox, t_ox, lam, res_mode, core_material)
-            
-            neff_te_mat[j, i] = res['neff_te']
-            neff_tm_mat[j, i] = res['neff_tm']
-            gamma_core_te_mat[j, i] = res['gamma_core_te']
-            gamma_core_tm_mat[j, i] = res['gamma_core_tm']
-            gamma_air_te_mat[j, i] = res['gamma_air_te']
-            gamma_air_tm_mat[j, i] = res['gamma_air_tm']
-            
-    return {
-        'vec1': vec1, 'vec2': vec2,
-        'param1_name': param1_name, 'param2_name': param2_name,
-        'gamma_core_te_mat': gamma_core_te_mat, 'gamma_core_tm_mat': gamma_core_tm_mat,
-        'gamma_air_te_mat': gamma_air_te_mat, 'gamma_air_tm_mat': gamma_air_tm_mat,
-        'neff_te_mat': neff_te_mat, 'neff_tm_mat': neff_tm_mat
-    }
+    if pol_choice
